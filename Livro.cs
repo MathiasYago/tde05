831110a92969863cf6abcd4954d52cac4afbe3fc
mathiasyago <mathias.santos@rede.ulbra.br>
2024-09-12 21:31:26 -0300

    first commit

---
+++ Livro.cs
@@ -0,0 +1,23 @@
+﻿
+class livro
+{
+    public string titulo;
+
+    public string autor;
+
+    public int ano;
+
+    public int paginas;
+
+    public void Informacoes()
+    {
+        Console.WriteLine($"Autor: {autor}\nTitulo: {titulo}\n Ano: {ano}\nPaginas: {paginas}");
+    }
+    public bool TemMaisDe300Paginas()
+    {
+       return paginas > 300;   
+    }
+
+}
+
+    
